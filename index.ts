fd6f228e1b4f5ce6f4cbb9dd81b4b2a4e8fa4d1b
borgesarch <gabrielborges.web@gmail.com>
2022-10-08 15:33:25 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,14 +2,12 @@ import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
 
 export = async () => {
-
     const vpc = new awsx.ec2.Vpc("eks-vpc", {
         cidrBlock: "10.0.0.0/16", 
         numberOfAvailabilityZones: 2, tags: { 
             name : 'eks-vpc'
         },
     })
-
     const publicSub = new awsx.ec2.Subnet("eks-sub-pub", vpc, {
         cidrBlock: '10.0.1.0/24',
     });
