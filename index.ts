50843cf3cfc5644107246e794957ab9c1342a803
borgesarch <gabrielborges.web@gmail.com>
2022-10-08 15:57:52 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,21 +8,11 @@ export = async () => {
             name : 'eks-vpc'
         },
     })
-    const publicSub = new awsx.ec2.Subnet("eks-sub-pub", vpc, {
-        cidrBlock: '10.0.1.0/24',
-    });
-
-    const privateSub = new awsx.ec2.Subnet("eks-sub-pri", vpc, {
-        cidrBlock: '10.0.4.0/24',
-    });
-
     const cluster = new eks.Cluster("eks-cluster", {
         vpcId: vpc.id,
         instanceType: "t2.medium",
         desiredCapacity: 2,
         minSize: 1,
-        subnetIds: [publicSub.id],
-        privateSubnetIds: [privateSub.id],
         maxSize: 2,
     });
 }
