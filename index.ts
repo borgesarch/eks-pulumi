abcd1679e1a2c4539443997bf0559f3f5bab92d3
borgesarch <gabrielborges.web@gmail.com>
2022-10-08 16:25:08 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,13 +2,8 @@ import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
 
 export = async () => {
- //   const vpc = new awsx.ec2.Vpc("eks-vpc", {
-  //      numberOfAvailabilityZones: 2, tags: { 
-  //          name : 'eks-dev-vpc'
-  //      },
-  //  })
+
     const cluster = new eks.Cluster("eks-dev", {
-    //    vpcId: vpc.id,
         instanceType: "t2.medium",
         desiredCapacity: 2,
         minSize: 1,
