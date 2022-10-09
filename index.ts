4abca0bad2bd47f8c8c4e06e4ce5e393442efea3
Gabriel Borges <gabrielborges.web@gmail.com>
2022-10-08 23:46:28 -0300

    fix(pulumi)

--- index.ts
+++ index.ts
@@ -1,19 +1,33 @@
 import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
+import * as pulumi from "@pulumi/pulumi";
 
-export = async () => {
+const projectName = pulumi.getProject();
 
-    const vpc = new awsx.ec2.Vpc("eks-sa-vpc", {
-        numberOfAvailabilityZones: 2, tags: { 
-            name : 'eks-sa-vpc'
-        },
-    })
-    const cluster = new eks.Cluster("eks-sa-dev", {
-        vpcId: vpc.id,
-        instanceType: "t2.medium",
-        desiredCapacity: 2,
-        minSize: 1,
-        maxSize: 2,
-    });
-}
+const cluster1 = new eks.Cluster(`${projectName}-1`);
+
+const vpc = new awsx.ec2.Vpc(`${projectName}-2`, {
+    tags: {"Name": `${projectName}-2`},
+});
+
+const cluster2 = new eks.Cluster(`${projectName}-2`, {
+    vpcId: vpc.id,
+    publicSubnetIds: vpc.publicSubnetIds,
+    desiredCapacity: 2,
+    minSize: 2,
+    maxSize: 2,
+    deployDashboard: false,
+    enabledClusterLogTypes: [
+        "api",
+        "audit",
+        "authenticator",
+    ],
+    vpcCniOptions: {
+        disableTcpEarlyDemux: true,
+    },
+});
+
+export const kubeconfig1 = cluster1.kubeconfig;
+export const kubeconfig2 = cluster2.kubeconfig;
+export const iamRoleArn = cluster1.core.clusterIamRole.arn;
 
