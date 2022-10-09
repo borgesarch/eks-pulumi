0666f2e1152248a2b0a4ef2864b7fd33245012e4
Gabriel Borges <gabrielborges.web@gmail.com>
2022-10-08 23:37:00 -0300

    fix(vpc)

--- index.ts
+++ index.ts
@@ -2,7 +2,14 @@ import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
 
 export = async () => {
-    const cluster = new eks.Cluster("eks-dev", {
+
+    const vpc = new awsx.ec2.Vpc("eks-sa-vpc", {
+        numberOfAvailabilityZones: 2, tags: { 
+            name : 'eks-sa-vpc'
+        },
+    })
+    const cluster = new eks.Cluster("eks-sa-dev", {
+        vpcId: vpc.id,
         instanceType: "t2.medium",
         desiredCapacity: 2,
         minSize: 1,
